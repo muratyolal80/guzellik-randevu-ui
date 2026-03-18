2617e2c3b415fcc9f1d3ae7b2edb739cccf8348c
Murat Yolal <murat.yolal80@gmail.com>
2026-03-18 12:44:10 +0300

    chore: sitemap güncellemeleri ve iyzipay java test dosyalarının temizlenmesi

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -1,12 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://guzellikrandevu.com',
+    siteUrl: process.env.SITE_URL || 'https://kuaforara.com.tr',
     generateRobotsTxt: true,
     sitemapSize: 7000,
     exclude: ['/owner/*', '/admin/*', '/auth/*', '/api/*'],
     robotsTxtOptions: {
         additionalSitemaps: [
-            `${process.env.SITE_URL || 'https://guzellikrandevu.com'}/server-sitemap.xml`,
+            `${process.env.SITE_URL || 'https://kuaforara.com.tr'}/server-sitemap.xml`,
         ],
     },
 }
